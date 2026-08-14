e7aa8807f41028e1ce33deba30744c264c0c42d9
MatheusDiasQueiroz <matheusdqueiroz@gmail.com>
2026-08-13 23:02:35 -0300

    feat: adiciona descrição dos algoritmos(apenas para compilação, não está totalmente escrita)

--- content/matheus/Geometry.h
+++ content/matheus/Geometry.h
@@ -1,14 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define pb push_back
-#define ll long long
-#define INF LLONG_MAX
-#define NINF LLONG_MIN
-#define full(a) a.begin(),a.end()
-#define get(a) for(int i=0; i<a.size(); i++){cin>>a[i];}
-#define double long double
-
+/**
+ * Author: Matheus Dias Queiroz
+ * Date: 13-08-2027
+ * License: no
+ * Source: folklore
+ * Description: Calculates a valid assignment to boolean variables a, b, c,... to a 2-SAT problem,
+ * so that an expression of the type $(a||b)\&\&(!a||c)\&\&(d||!b)\&\&...$
+ * becomes true, or reports that it is unsatisfiable.
+ * Negated variables are represented by bit-inversions (\texttt{\tilde{}x}).
+ * Usage:
+ *  TwoSat ts(number of boolean variables);
+ *  ts.either(0, \tilde3); // Var 0 is true or var 3 is false
+ *  ts.setValue(2); // Var 2 is true
+ *  ts.atMostOne({0,\tilde1,2}); // <= 1 of vars 0, \tilde1 and 2 are true
+ *  ts.solve(); // Returns true iff it is solvable
+ *  ts.values[0..N-1] holds the assigned values to the vars
+ * Time: O(N+E), where N is the number of boolean variables, and E is the number of clauses.
+ * Status: stress-tested
+ */
 const double PI = acos(-1);
 struct vetor{
     double x,y;
